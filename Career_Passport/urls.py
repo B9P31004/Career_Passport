139ebb0bef9fa4e58c299c40c7ad247721f2b079
b9p31004 <b9p31004@bunkyo.ac.jp>
2021-12-18 00:51:27 +0900

    Redo commit

--- Career_Passport/urls.py
+++ Career_Passport/urls.py
@@ -27,4 +27,3 @@ urlpatterns = [
     path('teacher_confirmation/',include('teacher_confirmation.urls')),
     path('file_uploader/',include('file_uploader.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
